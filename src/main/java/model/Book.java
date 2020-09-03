0bc2be4f5f623230c3991a501db14493f6ea9ac0
Jayden Kunwar <jaydenkunwar@Jaydens-MacBook-Pro.local>
2020-09-03 13:53:15 -0400

    initial commit

---
+++ src/main/java/model/Book.java
@@ -0,0 +1,90 @@
+package model;
+
+import java.util.Objects;
+
+public class Book {
+    private String title;
+    private String ISBN;
+    private String publisher;
+    private int year;
+    private String author; //assume each book only has one author
+
+    //need a constructor, setters, getters, and hash functions
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year &&
+                Objects.equals(title, book.title) &&
+                Objects.equals(ISBN, book.ISBN) &&
+                Objects.equals(publisher, book.publisher) &&
+                Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, ISBN, publisher, year, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", year=" + year +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    //go to generate, and you can create a constructor
+    public Book(String title, String ISBN, String publisher, int year, String author) {
+        this.title = title;
+        this.ISBN = ISBN;
+        this.publisher = publisher;
+        this.year = year;
+        this.author = author;
+    }
+}
